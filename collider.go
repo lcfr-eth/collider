c69a43fc86f224fb7c678cb5d3307b0bb810b3d7
lcfr <62533238+lcfr-eth@users.noreply.github.com>
2023-01-29 15:20:10 -0500

    Update collider.go

--- collider.go
+++ collider.go
@@ -26,20 +26,20 @@ const (
 )
 // found on stackoverflow https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
 func RandStringBytesMaskImprSrcUnsafe(n int) string {
-    var src = rand.NewSource(time.Now().UnixNano())
-    b := make([]byte, n)
-    for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-        if remain == 0 {
-            cache, remain = src.Int63(), letterIdxMax
-        }
-        if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-            b[i] = letterBytes[idx]
-            i--
-        }
-        cache >>= letterIdxBits
-        remain--
+  var src = rand.NewSource(time.Now().UnixNano())
+  b := make([]byte, n)
+  for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+    if remain == 0 {
+      cache, remain = src.Int63(), letterIdxMax
     }
-    return *(*string)(unsafe.Pointer(&b))
+    if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+      b[i] = letterBytes[idx]
+      i--
+    }
+    cache >>= letterIdxBits
+    remain--
+  }
+  return *(*string)(unsafe.Pointer(&b))
 }
 
 func randomReverseCapitalization(s string) string {
